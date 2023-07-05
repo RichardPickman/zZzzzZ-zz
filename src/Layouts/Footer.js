fe1080807c5adaf1508b26188c391ddb69e48fff
RichardPickman <drezzerock@gmail.com>
2023-07-05 17:14:44 +0200

    добавил инлайн стилей, чтобы переписать стили с пакета

--- src/Layouts/Footer.js
+++ src/Layouts/Footer.js
@@ -4,12 +4,13 @@ import { Col, Container, Row } from 'reactstrap';
 const Footer = () => {
     return (
         <React.Fragment>
-            <footer className="footer">
+            <footer
+                className="footer"
+                style={{ left: 0, right: 0 }}
+            >
                 <Container fluid>
                     <Row>
-                        <Col sm={6}>
-                            {new Date().getFullYear()} © Velzon.
-                        </Col>
+                        <Col sm={6}>{new Date().getFullYear()} © Velzon.</Col>
                         <Col sm={6}>
                             <div className="text-sm-end d-none d-sm-block">
                                 Design & Develop by Themesbrand
